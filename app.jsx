a52a71c0d8b88379b0b967dfd4fdbb090066a0ba
sumancode123 <sumank6314@gmail.com>
2026-05-09 18:14:19 +0530

    Initial commit for React project

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <img src={reactLogo} className="logo" alt="React logo" />
+      <h1>Hello React + Vite</h1>
+    </div>
+  )
+}
+
+export default App
